0d4b67c09ee02b8412e500aa91b6bddfaf6ea880
Arnaud Bellec <arnaud.bellec@applidget.com>
2013-04-08 18:39:33 +0200

    Factors out the variant class in a separate header

--- lundi.hpp
+++ lundi.hpp
@@ -1,46 +1,17 @@
 #pragma once
 
 #include <vector>
-#include <boost/variant.hpp>
 #include <boost/fusion/container/vector.hpp>
 #include <boost/fusion/view/reverse_view.hpp>
 #include <boost/fusion/algorithm/iteration/for_each.hpp>
 #include <boost/fusion/functional/invocation/invoke.hpp>
 #include <lua.hpp>
 
-#include "lundi/nil.hpp"
+#include "lundi/variant.hpp"
 
 namespace lua {
 
-typedef boost::variant<signed int, double, bool, char const*, nil> variant;
-
 namespace detail {
-
-class push_variant : public boost::static_visitor<> {
-    lua_State *state_;
-public:
-    push_variant(lua_State *state) : state_(state) {}
-
-    void operator()(signed int i) const {
-        lua_pushinteger(state_, i);
-    }
-
-    void operator()(double d) const {
-        lua_pushnumber(state_, d);
-    }
-
-    void operator()(bool b) const {
-        lua_pushboolean(state_, b);
-    }
-
-    void operator()(char const *szc) const {
-        lua_pushstring(state_, szc);
-    }
-
-    void operator()(nil n) const {
-        lua_pushnil(state_);
-    }
-};
 
 class function_wrapper {
 public:
---
+++ lundi/variant.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <boost/variant.hpp>
+#include <lua.hpp>
+#include "lundi/nil.hpp"
+
+namespace lua {
+
+typedef boost::variant<signed int, double, bool, char const*, nil> variant;
+
+namespace detail {
+
+class push_variant : public boost::static_visitor<> {
+    lua_State *state_;
+public:
+    push_variant(lua_State *state) : state_(state) {}
+
+    void operator()(signed int i) const {
+        lua_pushinteger(state_, i);
+    }
+
+    void operator()(double d) const {
+        lua_pushnumber(state_, d);
+    }
+
+    void operator()(bool b) const {
+        lua_pushboolean(state_, b);
+    }
+
+    void operator()(char const *szc) const {
+        lua_pushstring(state_, szc);
+    }
+
+    void operator()(nil n) const {
+        lua_pushnil(state_);
+    }
+};
+
+} // detail
+} // lua
