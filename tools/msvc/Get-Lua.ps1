c1d7f1889d48ebd09b30da8f10a20fd5f49fc8ed
Arnaud Bellec <arnaud.bellec@gmail.com>
2013-07-20 03:37:38 +0200

    Force the output directory in the PS downloader

--- tools/msvc/Get-Lua.ps1
+++ tools/msvc/Get-Lua.ps1
@@ -3,12 +3,14 @@ Param(
     [string]$version
 )
 
+$myPath = (split-path -parent $MyInvocation.MyCommand.Definition) + "`\"
+
 $filename = "lua-" + $version + ".tar.gz"
 $url = "http://www.lua.org/ftp/" + $filename
 
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $filename)
+$wc.DownloadFile($url, $myPath + $filename)
 
-C:\Program` Files\7-Zip\7z.exe e $filename
-$tar = "lua-" + $version + ".tar"
+C:\Program` Files\7-Zip\7z.exe e ($myPath + $filename)
+$tar = $myPath + "lua-" + $version + ".tar"
 C:\Program` Files\7-Zip\7z.exe -r -o"..\\.." x $tar
